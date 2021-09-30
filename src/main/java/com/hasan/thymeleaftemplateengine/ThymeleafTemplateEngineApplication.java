37489ad542712a62acc43883d73a6b63496d6a72
Hasan <hasanuzzaman.dev@gmail.com>
2021-09-30 15:40:43 +0600

    Initial commit

---
+++ src/main/java/com/hasan/thymeleaftemplateengine/ThymeleafTemplateEngineApplication.java
@@ -0,0 +1,13 @@
+package com.hasan.thymeleaftemplateengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTemplateEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafTemplateEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasan/thymeleaftemplateengine/ThymeleafTemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.thymeleaftemplateengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTemplateEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
